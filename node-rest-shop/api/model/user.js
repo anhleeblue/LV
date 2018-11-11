5053f0c58016f827dfcde4570983f7ae2d00fc01
anhleeblue <anhleeblue@gmail.com>
2018-11-12 02:44:39 +0700

    Admin Product

--- node-rest-shop/api/model/user.js
+++ node-rest-shop/api/model/user.js
@@ -5,7 +5,9 @@ const userSchema = mongoose.Schema({
     email:{ type: String,required: true, unique:true,
     match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password:{ type: String,required: true}
+    password:{ type: String,required: true},
+    userName:{type: String},
+    
 });
 
 module.exports = mongoose.model('User ', userSchema);
--- node-rest-shop/api/routes/users.js
+++ node-rest-shop/api/routes/users.js
@@ -23,7 +23,8 @@ router.post('/signup', (req, res, next) => {
                     const user = new User({
                         _id: new mongoose.Types.ObjectId(),
                         email: req.body.email,
-                        password: hash
+                        password: hash,
+                        userName:req.body.userName
                      });
                      user.save()
                      .then(result => {
